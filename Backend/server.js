a03d8212da7d4b48f56d665c8bafd99a83e808e0
DiegoBeniel <a23300718@ceti.mx>
2026-06-19 22:35:44 -0600

    Separación frontend y backend

---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const conectarDB = require('./config/db');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.static('../Frontend/public'));
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/datos', require('./routes/datos'));
+app.use('/api/admin', require('./routes/admin'));
+app.use('/api/gerente', require('./routes/gerente'));
+
+// Ruta para servir el frontend al entrar a la raíz
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/../Frontend/public/index.html');
+});
+
+// Conectar a MongoDB  y luego iniciar el servidor
+conectarDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+});
